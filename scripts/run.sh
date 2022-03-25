e1c9144d0a3e2ebd7faac8d567d542e900ad4580
Chunfeng Song <developfeng@gmail.com>
2022-03-25 15:02:56 +0800

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,8 +5,12 @@ python main.py train --config-path configs/voc12_bcm.yaml
 #FR-Shifting Training
 python main.py train --config-path configs/voc12_fr.yaml
 
+#Testing
+python main.py test --config-path configs/voc12_fr.yaml --model-path data/models/voc12/FR/checkpoint_final.pth
+
 #Generating Pseudo Labels for Semantic Segmenation
-python main.py gen --config-path configs/voc12_fr.yaml --model-path data/models/voc12/deeplabv2_resnet101_msc_bcm/trainaug/checkpoint_final.pth
+python main.py test --config-path configs/voc12_fr.yaml --model-path data/models/voc12/FR/checkpoint_final.pth --gen-training True
+python main.py gen --config-path configs/voc12_fr.yaml
 
 #Generating Pseudo Labels for Instance Segmenation
-python main.py crf --config-path configs/voc12_crf.yaml
+python make_coco_inst_mask_label.py
